c56266833725ece0cc40e56ac7a323dcae567bd7
balikfromua <yuhimenko.valentin@gmail.com>
2019-09-12 15:17:26 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as tf from '@tensorflow/tfjs';
+
+class MainApp extends React.Component {
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+   componentDidMount() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "user"
+          }
+        })
+        .then(stream => {
+          window.stream = stream;
+          this.videoRef.current.srcObject = stream;
+          return new Promise((resolve, reject) => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+
+       const modelPromise = tf.loadLayersModel('./model/model.json');
+
+      Promise.all([modelPromise, webCamPromise])
+        .then(values => {
+          this.detectFrame(this.videoRef.current, values[0]);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+
+  detectFrame = (video, model) => {
+    console.log(video)
+
+    let tensor = tf.fromPixels(video)
+    .resizeNearestNeighbor([224, 224])
+    .toFloat();
+
+    model.predict(tensor).then(predictions => {
+      console.log(this.renderPredictions(predictions));
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
+      });
+    });
+  };
+
+  renderPredictions = predictions => {
+    console.log(predictions);
+  };
+
+  render() {
+    return (
+      <div>
+        <video
+          className="size"
+          autoPlay
+          playsInline
+          muted
+          ref={this.videoRef}
+          width="600"
+          height="500"
+        />
+        <canvas
+          className="size"
+          ref={this.canvasRef}
+          width="600"
+          height="500"
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MainApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
